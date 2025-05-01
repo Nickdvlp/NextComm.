39ca0e957d9d23335a1e3c2388df00ff34854957
nikhil sharma <nikhil.sharma3755@gmail.com>
2025-05-01 09:03:10 +0530

    app main

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -17,66 +17,60 @@ import UserManagement from "./components/admin/UserManagement";
 import ProductManagement from "./components/admin/ProductManagement";
 import EditProduct from "./components/admin/EditProduct";
 import OrderManagement from "./components/admin/OrderManagement";
-import { Provider, useSelector } from "react-redux";
-import store from "./redux/store";
+import { useSelector } from "react-redux";
 
 function App() {
   const { user } = useSelector((state) => state.auth);
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<UserLayout />}>
-            <Route index element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route
-              path="/collections/:collection"
-              element={<CollectionPage />}
-            />
-            <Route path="/product/:id" element={<ProductDetails />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route
-              path="/order-confirmation"
-              element={<OrderConfirmationPage />}
-            />
-            <Route path="/order/:id" element={<OrderDetails />} />
-            <Route path="/my-orders" element={<MyOrdersPage />} />
-          </Route>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/collections/:collection" element={<CollectionPage />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/checkout" element={<Checkout />} />
           <Route
-            path="/admin"
-            element={
-              user?.role === "admin" ? (
-                <AdminLayout />
-              ) : (
-                <Navigate to="/login" replace />
-              )
-            }
-          >
-            <Route index element={<AdminHomePage />} />
-            <Route path="users" element={<UserManagement />} />
-            <Route path="products" element={<ProductManagement />} />
-            <Route path="products/:id/edit" element={<EditProduct />} />
-            <Route path="orders" element={<OrderManagement />} />
-          </Route>
-        </Routes>
+            path="/order-confirmation"
+            element={<OrderConfirmationPage />}
+          />
+          <Route path="/order/:id" element={<OrderDetails />} />
+          <Route path="/my-orders" element={<MyOrdersPage />} />
+        </Route>
+        <Route
+          path="/admin"
+          element={
+            user?.role === "admin" ? (
+              <AdminLayout />
+            ) : (
+              <Navigate to="/login" replace />
+            )
+          }
+        >
+          <Route index element={<AdminHomePage />} />
+          <Route path="users" element={<UserManagement />} />
+          <Route path="products" element={<ProductManagement />} />
+          <Route path="products/:id/edit" element={<EditProduct />} />
+          <Route path="orders" element={<OrderManagement />} />
+        </Route>
+      </Routes>
 
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick={false}
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-          transition={Bounce}
-        />
-      </BrowserRouter>
-    </Provider>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+      />
+    </BrowserRouter>
   );
 }
 
